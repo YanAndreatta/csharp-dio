ffa3ff15b23c12efda9cb32c81e813e372325a57
YanAndreatta <yan.duca2@gmail.com>
2024-05-08 16:15:10 -0300

    Declaring variable types

--- Program.cs
+++ Program.cs
@@ -6,8 +6,27 @@ dio_bootcamp.models.Pessoa p = new dio_bootcamp.models.Pessoa();
 
 */
 
-Pessoa pessoa1 = new Pessoa();
+
+string apresentacao = "Olá, seja bem vindo";
+
+int quantidade = 1;
+
+double altura = 1.80;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("valor da variável quantidade: " + quantidade);
+Console.WriteLine("valor da variável altura: " + altura.ToString("0.00"));
+Console.WriteLine("valor da variável preco: " + preco);
+Console.WriteLine("valor da variável condicao: " + condicao);
+
+
+/* Pessoa pessoa1 = new Pessoa();
 
 pessoa1.Nome = "Yan";
 pessoa1.Idade = 27;
 pessoa1.Apresentar();
+ */
