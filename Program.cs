0cd3282499642121b0eb258140fb6d91b2c911d8
Yan Andreatta <yan.duca2@gmail.com>
2024-05-10 11:19:47 -0300

    To interrupt the execution flow

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ int contador = 1;
 while (contador <= 10) {
     Console.WriteLine($"{contador} Execução: {numero} x {contador} = {numero * contador}");
     contador++;
+
+    if( contador == 6) {
+        break;
+    }
 }
 
 
