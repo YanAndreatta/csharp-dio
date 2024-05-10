94458e6d0ed8e7bcdb6679c4323196e13e9f7b6e
Yan Andreatta <yan.duca2@gmail.com>
2024-05-10 11:33:24 -0300

    Do-While

--- Program.cs
+++ Program.cs
@@ -1,23 +1,38 @@
 ﻿using dio_bootcamp.models;
 
 
-int numero = 5;
-int contador = 1; 
 
 
-while (contador <= 10) {
-    Console.WriteLine($"{contador} Execução: {numero} x {contador} = {numero * contador}");
-    contador++;
+int soma = 0, numero = 0;
 
-    if( contador == 6) {
-        break;
-    }
-}
+do {
+    Console.WriteLine("Digite um número (0 para parar)");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    soma += numero;
+
+} while (numero != 0);
+
+Console.WriteLine($"Total da soma dos números é {soma}");
 
 
 
 
 
+
+
+// int numero = 5;
+// int contador = 1; 
+
+
+// while (contador <= 10) {
+//     Console.WriteLine($"{contador} Execução: {numero} x {contador} = {numero * contador}");
+//     contador++;
+
+//     if( contador == 6) {
+//         break;
+//     }
+// }
 
 
 
