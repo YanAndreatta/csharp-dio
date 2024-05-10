69f0e7e0f3997b97fa4415997bc48b793f411330
Yan Andreatta <yan.duca2@gmail.com>
2024-05-10 13:32:35 -0300

    Arrays

--- dio-bootcamp/Program.cs
+++ dio-bootcamp/Program.cs
@@ -1,10 +1,33 @@
 ﻿using dio_bootcamp.Common.models;
 
-Pessoa pessoa = new Pessoa();
 
-pessoa.Nome = "Yan";
-pessoa.Idade = 20;
-pessoa.Apresentar();
+
+int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+
+
+for (int i = 0; i < arrayInteiros.Length; i++) {
+    Console.WriteLine($"Posição n. {i} - {arrayInteiros[i]}");
+}
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa pessoa = new Pessoa();
+
+// pessoa.Nome = "Yan";
+// pessoa.Idade = 20;
+// pessoa.Apresentar();
 
 
 // string opcao = "0";
